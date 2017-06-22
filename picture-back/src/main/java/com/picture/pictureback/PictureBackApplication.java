af7975045ecb8fc76373b2684b0bc3844339db3b
David Milaveau <david.milaveau@hotmail.fr>
2017-06-22 12:25:28 +0200

    Initial Commit
    Add back repository

---
+++ picture-back/src/main/java/com/picture/pictureback/PictureBackApplication.java
@@ -0,0 +1,12 @@
+package com.picture.pictureback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureBackApplication.class, args);
+	}
+}
