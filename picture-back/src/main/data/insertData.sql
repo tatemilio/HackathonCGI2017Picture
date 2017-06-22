0a6c2b8f144eee5d07f1238d5e0288167d5421e6
Dutrevis <patrick.dutrevis@cgi.com>
2017-06-22 21:49:29 +0200

    data

---
+++ picture-back/src/main/data/insertData.sql
@@ -0,0 +1,15 @@
+COPY picture_user FROM 'c:\temp\picture_user.csv';
+COPY mood FROM 'c:\temp\mood.csv';
+
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
+select nextval('hibernate_sequence');
