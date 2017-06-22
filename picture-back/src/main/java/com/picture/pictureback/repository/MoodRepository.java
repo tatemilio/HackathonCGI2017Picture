5cb3e63044c88203a36520e8b18ecbc27d6cec16
Dutrevis <patrick.dutrevis@cgi.com>
2017-06-22 21:41:35 +0200

    yy

--- picture-back/src/main/java/com/picture/pictureback/repository/MoodRepository.java
+++ picture-back/src/main/java/com/picture/pictureback/repository/MoodRepository.java
@@ -3,6 +3,7 @@ package com.picture.pictureback.repository;
 import com.picture.pictureback.domain.Mood;
 import com.picture.pictureback.domain.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
