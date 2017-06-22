ab80f7baebccb5cf76fd7693691f9e74fe8b1cba
David Milaveau <david.milaveau@hotmail.fr>
2017-06-22 17:13:38 +0200

    Auto stash before rebase of "origin/master"

--- picture-back/src/main/java/com/picture/pictureback/domain/PictureUserRepository.java
+++ picture-back/src/main/java/com/picture/pictureback/domain/PictureUserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface PictureUserRepository extends CrudRepository<PictureUser, Long> {
 
+    public PictureUser findByUsername(String username);
+
 }
