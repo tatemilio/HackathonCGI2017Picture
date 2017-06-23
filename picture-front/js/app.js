8adfa8780ca8dbf4eb20b1e980e7efb2e9d8b1a2
Ivan ROPITEAUX <ivan.ropiteaux_ext@michelin.com>
2017-06-23 15:25:53 +0200

    Update

--- picture-front/js/app.js
+++ picture-front/js/app.js
@@ -25,6 +25,7 @@ angular.module('picture', [
   'picture.pollFeedbackService',
   'picture.pollService',
   'picture.homeController',
+  'picture.tvController',
   'picture.pollController',
   'picture.welcomeController',
   'picture.registerController',
